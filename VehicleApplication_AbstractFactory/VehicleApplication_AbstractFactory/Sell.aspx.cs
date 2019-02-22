558b1d6ae39d94f7baeabc4eb4046c9e83cfdf08
SimonPrimetzhofer <simon.primetzhofer@live.at>
2019-02-22 12:07:46 +0100

    Selling vehicles works

--- VehicleApplication_AbstractFactory/VehicleApplication_AbstractFactory/Sell.aspx.cs
+++ VehicleApplication_AbstractFactory/VehicleApplication_AbstractFactory/Sell.aspx.cs
@@ -6,9 +6,12 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using VehicleApplication_AbstractFactory.AbstractFactory;
 
 namespace VehicleApplication_AbstractFactory {
     public partial class Sell : System.Web.UI.Page {
+
+        AbstractVehicleFactory avf = FactoryProducer.GetVehicleFactory();
 
         SqlConnection con;
 
@@ -29,10 +32,18 @@ namespace VehicleApplication_AbstractFactory {
                 //Vehicle was selected and is not the default vehicle
                 if (VehicleDropdown.SelectedValue.Length > 0 && VehicleDropdown.SelectedValue != "DefaultVehicle") {
 
+                    SqlDataAdapter adapter = new SqlDataAdapter("", con);
+                    adapter.UpdateCommand = con.CreateCommand();
+                    adapter.UpdateCommand.CommandText = "UPDATE dbo.Vehicle SET Owner=@Owner WHERE ID=@ID";
+                    adapter.UpdateCommand.Parameters.AddWithValue("@Owner", BuyerDropdown.SelectedValue);
+                    adapter.UpdateCommand.Parameters.AddWithValue("@ID", VehicleDropdown.SelectedValue);
 
+                    adapter.UpdateCommand.ExecuteNonQuery();
 
                 }
             }
+
+            ClearForm();
 
             con.Close();
         }
@@ -59,6 +70,16 @@ namespace VehicleApplication_AbstractFactory {
             VehicleDropdown.DataBind();
 
             con.Close();
+        }
+
+        private void ClearForm() {
+
+            SellerDropdown.SelectedIndex = 0;
+            BuyerDropdown.SelectedIndex = 0;
+            VehicleDropdown.DataSource = new DataTable();
+            VehicleDropdown.DataBind();
+            Price.Text = "";
+
         }
     }
 }
