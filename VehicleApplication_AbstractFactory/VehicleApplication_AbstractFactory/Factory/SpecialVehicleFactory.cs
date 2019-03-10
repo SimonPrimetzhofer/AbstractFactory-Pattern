652af020230c7640ae7c4664d3ab4431acd06fac
SimonPrimetzhofer <simon.primetzhofer@live.at>
2019-03-10 14:35:48 +0100

    SpecialVehicleFactory to show substitution of concrete factory

---
+++ VehicleApplication_AbstractFactory/VehicleApplication_AbstractFactory/Factory/SpecialVehicleFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VehicleApplication_AbstractFactory.AbstractFactory;
+using VehicleApplication_AbstractFactory.Model;
+
+namespace VehicleApplication_AbstractFactory.Factory {
+    public class SpecialVehicleFactory : IAbstractFactory<Vehicle> {
+        
+        public Vehicle Get(string type) {
+            Vehicle v = null;
+
+            switch(type) {
+                case "PKW": v = new Car(); break;
+                case "LKW": v = new Truck(); break;
+                case "Motorrad": v = new Motorcycle(); break;
+                case "Traktor": v = new Tractor(); break;
+            }
+
+            return v;
+        }
+    }
+}
